d4173052126945a3ee52933a370f943e1590101b
Sudip Singh <sudipsingh702@gmail.com>
2023-01-02 10:50:53 +0530

    sudip second

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -297,7 +297,7 @@ class _HomeState extends State<Home> {
           heightSpace,
           heightSpace,
 
-          feedback(),
+          
           Row(
             children: [
               Container(
